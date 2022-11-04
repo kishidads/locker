09dca1fd1f9e749b1b139939d3ccab3ef32ffd02
GuilhermeRehder <guilhermekishida@gmail.com>
2022-11-03 21:50:09 -0300

    Refinamento da seleção de armários; pequenas alterações no script do banco de dados.

--- connection/Connection.php
+++ connection/Connection.php
@@ -6,7 +6,7 @@ class Connection {
 
    public static function getConnection() {
         if (!isset(self::$instance)) {
-            self::$instance = new PDO('mysql:host=localhost;dbname=bdlocker', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance = new PDO('mysql:host=localhost;dbname=dblocker', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
--- view/armarios/armarios.php
+++ view/armarios/armarios.php
@@ -15,8 +15,8 @@
     <section class="lockers">
         <div class="section-header">
             <h1>Armários</h1>
-            <form action="/armarios" method="POST">
 
+            <form action="/armarios" method="POST">
                 <label for="local">Local</label>
 
                 <select name="local" id="local" required onchange="if(this.value != 0) { this.form.submit(); }">
@@ -25,10 +25,11 @@
                     <option value="quimica">Corredor Química</option>
                     <option value="nutricao">Corredor Nutrição</option>
                 </select>
-                <input type='hidden' name='listar' value='listar'>
 
+                <input type='hidden' name='listar' value='listar'>
             </form>
         </div>
+
         <div class="lockers-grid-container">
             <div class="lockers-grid">
                 <?php
@@ -37,12 +38,18 @@
                     $secao = $armarios[0]->getSecao();
 
                     foreach ($armarios as $armario) {
+
                         if ($secao != $armario->getSecao()) {
+
                             echo "</div>";
                             echo "<div class='lockers-grid'>";
+
                             $secao = $armario->getSecao();
+
                         }
+
                         $dom = new DOMDocument();
+
                         $div = $dom->createElement("div", "{$armario->getSecao()}{$armario->getNumero()}");
                         $div->setAttribute("class", "grid-item {$armario->getSituacao()}");
                         $dom->appendChild($div);
@@ -52,9 +59,34 @@
                 ?>
             </div>
         </div>
+
+        <form class="reserve-form" action="/checkout" method="GET">
+            <input class="reserve" type="hidden">
+            <button>Avançar</button>
+        </form>
     </section>
     
-    <?php include "public/footer.html" ?>               
-    <script src="script.js"></script>
+    <?php include "public/footer.html" ?>
+
+    <script>
+        const div = document.querySelectorAll(".disponivel");
+        const input = document.querySelector(".reserve");
+
+        for (let i = 0; i < div.length; i++) {         
+              
+            div[i].onclick = function() {
+
+                div.forEach(div => {
+                    div.classList.remove("selected");
+                });
+
+                div[i].classList.add("selected");
+
+                input.value = div[i].innerHTML;
+
+            }
+
+        }
+    </script>
 </body>
 </html>
